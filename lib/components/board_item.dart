4c45cef2e7593b0f46f6457e9b46441b9ca021d7
Javier E. <jaedns@gmail.com>
2026-07-31 18:11:33 -0300

    Refactor item components to use Material InkWell overlay
    Update BoardItem and CardItem to use a Positioned.fill Material widget for
    InkWell effects. Additionally, increase the alpha values for drag-related
    placeholder containers and update image caching configurations.

--- lib/components/board_item.dart
+++ lib/components/board_item.dart
@@ -2,6 +2,7 @@ import 'package:bofe/build_context.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
 import '../graphql/schema.graphql.dart';
 import '../graphql/fragments/board_item_fragment.graphql.dart';
 import 'user_item.dart';
@@ -26,63 +27,71 @@ class BoardItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    return InkWell(
-      borderRadius: BorderRadius.circular(12),
-      onTap: () => context.router.pushToBoard(board),
-      child: Ink(
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12), color: colorScheme.onInverseSurface),
-        child: Stack(
-          alignment: Alignment.center,
-          clipBehavior: Clip.hardEdge,
-          children: [
-            if (board.backgroundImageAttachment != null)
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: CachedNetworkImage(
-                  imageUrl: board.backgroundImageAttachment!.thumbnailUrl!.toString(),
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: double.infinity,
+    return Container(
+      decoration: BoxDecoration(
+        color: colorScheme.onInverseSurface,
+        borderRadius: borderRadius12,
+        boxShadow: kElevationToShadow[1],
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.hardEdge,
+        children: [
+          if (board.backgroundImageAttachment != null)
+            ClipRRect(
+              borderRadius: borderRadius12,
+              child: CachedNetworkImage(
+                imageUrl: board.backgroundImageAttachment!.thumbnailUrl!.toString(),
+                useOldImageOnUrlChange: true,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: borderRadius12,
+              color: colorScheme.onInverseSurface.withValues(alpha: 0.5),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              spacing: 4,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  board.name,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
                 ),
-              ),
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: colorScheme.onInverseSurface.withValues(alpha: 0.5),
-              ),
+                Text(board.description, maxLines: 3, overflow: TextOverflow.ellipsis, textAlign: TextAlign.center),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                spacing: 4,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    board.name,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  Text(board.description, maxLines: 3, overflow: TextOverflow.ellipsis, textAlign: TextAlign.center),
-                ],
-              ),
+          ),
+          Positioned(
+            left: 12,
+            bottom: 12,
+            right: 12,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                UserItem(user: board.user),
+                _getVisibilityIcon(),
+              ],
             ),
-            Positioned(
-              left: 12,
-              bottom: 12,
-              right: 12,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  UserItem(user: board.user),
-                  _getVisibilityIcon(),
-                ],
-              ),
+          ),
+          Positioned.fill(
+            child: Material(
+              color: Colors.transparent,
+              borderRadius: borderRadius12,
+              child: InkWell(borderRadius: borderRadius12, onTap: () => context.router.pushToBoard(board)),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
